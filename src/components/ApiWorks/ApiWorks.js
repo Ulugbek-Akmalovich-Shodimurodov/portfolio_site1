ab425e3f4dbc4ce2b157bf28766e38a2f5d86da5
Ulugbek-Akmalovich-Shodimurodov <99031894+Ulugbek-Akmalovich-Shodimurodov@users.noreply.github.com>
2023-04-12 21:07:19 +0500

    qwerty

--- src/components/ApiWorks/ApiWorks.js
+++ src/components/ApiWorks/ApiWorks.js
@@ -5,22 +5,16 @@ import './apiWorks.css'
 
 function ApiWorks() {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
     axios.get('https://jsonplaceholder.typicode.com/users')
       .then(response => {
         setData(response.data);
-        setLoading(false);
       })
       .catch(error => {
         console.error(error);
-        setLoading(false);
       });
   }, []);
-
-  console.log(data);
 
   return (
     <div className='api-box'>
